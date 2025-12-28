9d07ce78cd2fb92ff82ce60e8bb7d1a3b375ad44
Leandro Sybilla Borges Pedro <leandro@sybilla.com.br>
2025-12-28 16:21:46 +0000

    Adding nodejs backend. Creating group6 namespace.

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+import { WebSocketServer } from 'ws';
+import RabbitMQ from 'amqplib';
+
+const socketList = [];
+const rabbitMqUrl = `amqp://${process.env.RABBIT_USER}:${process.env.RABBIT_PASS}@${process.env.RABBIT_URL}:${process.env.RABBIT_PORT}`;
+
+const server = new WebSocketServer({ port: 8000 });
+
+server.on('connection', (socket) => {
+  console.log('Client connected');
+  socketList.push(socket);
+  
+  socket.on('message', (message) => {
+    console.log(`Received: ${message}`);
+    socket.send(`Server: ${message}`);
+  });
+
+  socket.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+console.log('WebSocket server is running on port 8000');
+
+const do_consume = async () => {
+  const conn = await RabbitMQ.connect(rabbitMqUrl, "heartbeat=60");
+  const channel = await conn.createChannel()
+  const queueName = 'group6';
+  await conn.createChannel();
+  await channel.assertQueue(queueName, {durable: false});
+  await channel.consume(queueName, async (msg) => {
+    if (msg !== null) {
+      for await (const socket of socketList) {
+        await socket.send(msg.content.toString());
+      }
+      channel.ack(msg);
+    } else {
+      console.log('Consumer cancelled by server');
+    }
+  });
+}
+
+(async () => {
+  do_consume();
+})();
