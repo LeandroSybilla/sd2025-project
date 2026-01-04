73172f6b81216440d0f6a0eafe3fddf4cbe1d6f0
aleuni <2112821@student.uma.pt>
2026-01-04 22:19:55 +0000

    update metric Rece Processing Duration

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const raceProcessedMessages = new client.Counter({
 const raceProcessingDuration = new client.Histogram({
   name: 'race_processing_duration_seconds',
   help: 'Duration of message processing in seconds',
-  buckets: [0.1, 0.5, 1, 2],
+  buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5],
   registers: [register]
 });
 
